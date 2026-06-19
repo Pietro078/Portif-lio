8237dc038ee9b1d51b0a426c08655080509e0a11
Salsa e Cebolinha <pietro.s.thor@gmail.com>
2026-06-19 16:43:41 -0300

    Switch database configuration to use dj_database_url

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -2,6 +2,7 @@
 Configurações do projeto Portfólio.
 """
 
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -64,13 +65,13 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 ASGI_APPLICATION = "portfolio.asgi.application"
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600
+    )
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
